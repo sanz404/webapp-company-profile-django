728d995ccb4b235a3aa57c365e7a003cd12311a6
Sandy Andryanto <sandyandryanto@gmail.com>
2022-07-23 14:15:28 +0700

    up

--- backend/app_main/api_app/models.py
+++ backend/app_main/api_app/models.py
@@ -1,9 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Country(models.Model):
+    code = models.CharField(max_length=64)
+    name = models.CharField(max_length=64)
+    
+class Person(models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=False)
+    phone = models.CharField(max_length=64, null=True)
+    city = models.CharField(max_length=64, null=True)
+    zip_code = models.CharField(max_length=64, null=True)
+    address1 = models.TextField(null=True)
+    address2 = models.TextField(null=True)
+    is_admin = models.BooleanField(default=False)
+    status = models.BooleanField(default=False)
+
 class Contact(models.Model):
     name = models.CharField(max_length=64)
     email = models.CharField(max_length=64)
-    phone = models.CharField(max_length=64)
-    website = models.CharField(max_length=64)
-    address = models.CharField(max_length=255)
+    phone = models.CharField(max_length=64,null=True)
+    website = models.CharField(max_length=64,null=True)
+    address = models.TextField(blank = True)
